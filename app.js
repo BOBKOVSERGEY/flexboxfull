89e5e21a51b9a3b196b238636a6680950c0086ba
BOBKOVSERGEY <sergey_bobkov@inbox.ru>
2017-07-05 17:57:38 +0300

    align-item align self

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var bs = require("browser-sync").create();
 
-bs.watch(['dist/*.*']).on("change", bs.reload);
+bs.watch(['dist/*.*', 'dist/*.scss']).on("change", bs.reload);
 
 // Start a Browsersync proxy
 bs.init({
